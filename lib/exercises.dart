c110f32862547f42e3b2bb4e7e7f3e111522ea75
Andy <Anmile@ucsc.edu>
2021-04-28 14:21:04 -0700

    Update exercises.dart
    removed unnecessary packages

--- lib/exercises.dart
+++ lib/exercises.dart
@@ -1,6 +1,6 @@
-library pages;
+//library pages;
 
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
