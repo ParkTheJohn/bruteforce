e65c802e176e7bda76d6ef3d3aca445aa27d730e
su2021-p <sushekar@ucsc.edu>
2021-05-16 17:35:03 -0700

    Added button that displays selectable dummy logged exercises.

--- lib/myLogsFireStoreVersion.dart
+++ lib/myLogsFireStoreVersion.dart
@@ -1,26 +1,25 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cse_115a/createWorkout.dart';
 import 'package:cse_115a/main.dart';
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'workoutPage.dart';
 
 class WorkoutInfo {
   final int weight;
   final DateTime realTimeStamp;
-  final String colorVal;
 
-  WorkoutInfo(this.weight, this.realTimeStamp, this.colorVal);
+  WorkoutInfo(this.weight, this.realTimeStamp);
 
   WorkoutInfo.fromMap(Map<String, dynamic> map)
       : assert(map['weight'] != null),
-        assert(map['realTimeStamp']!=null),
-        assert(map['colorVal'] != null),
+        assert(map['realTimeStamp'] != null),
         weight = map['weight'],
-        realTimeStamp = map['realTimeStamp'].toDate(),
-        colorVal = map['colorVal'];
+        realTimeStamp = map['realTimeStamp'].toDate();
 
   @override
-  String toString() => "Record<$weight:$realTimeStamp:$colorVal>";
+  String toString() => "Record<$weight:$realTimeStamp>";
 }
 
 class WorkoutInfoHomePage extends StatefulWidget {
@@ -54,40 +53,78 @@ class _WorkoutInfoHomePageState extends State<WorkoutInfoHomePage> {
       charts.Series(
         domainFn: (WorkoutInfo info, _) => info.realTimeStamp,
         measureFn: (WorkoutInfo info, _) => info.weight,
-        colorFn: (WorkoutInfo info, _) =>
-            charts.ColorUtil.fromDartColor(Color(int.parse(info.colorVal))),
+        colorFn: (_, __) =>
+        charts.MaterialPalette.blue.shadeDefault,
         id: 'WorkoutDetails',
         data: myData,
       ),
     );
+  }
 
+  Widget buildButton(BuildContext context) {
+    return ListView(padding: const EdgeInsets.all(1), children: <Widget>[
+      Container(
+          child: ElevatedButton(
+              child: Text('Display an exercise!'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => DisplayCompletedLogs()),
+                );
+              })),
+    ]);
   }
 
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: DefaultTabController(
-            length: 2,
-            child: Scaffold(
-              appBar: AppBar(
-                flexibleSpace: TabBar(
-                  indicatorColor: Color(0xff9962D0),
-                  tabs: [
-                    Tab(
-                      icon: Icon(FontAwesomeIcons.solidChartBar),
+
+      return MaterialApp(
+
+        home: Scaffold(
+            body: ListView(
+
+                children: <Widget>[
+
+                  SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: buildButton(context)
+
+                  ),
+
+
+
+                SizedBox(
+                    width: 320.0,
+                    height:371.0,
+                    child: DefaultTabController(
+                length: 2,
+                child: Scaffold(
+                  appBar: AppBar(
+                    flexibleSpace: TabBar(
+                      indicatorColor: Color(0xff9962D0),
+                      tabs: [
+                        Tab(
+                          icon: Icon(FontAwesomeIcons.solidChartBar),
+                        ),
+                        Tab(icon: Icon(FontAwesomeIcons.chartLine)),
+                      ],
                     ),
-                    Tab(icon: Icon(FontAwesomeIcons.chartLine)),
-                  ],
-                ),
-              ),
-              body: TabBarView(
-                  children:[
+                  ),
+                  body: TabBarView(
+                      children: [
 
-                    _buildBodyChart(context), _buildBodyLine(context)
+                        _buildBodyChart(context), _buildBodyLine(context)
 
-                  ]),
-            )));
+                      ]),
+                )))
+                ]
+        ),
+        ),
+
+      );
   }
 
   Widget _buildBodyLine(BuildContext context) {
@@ -99,7 +136,6 @@ class _WorkoutInfoHomePageState extends State<WorkoutInfoHomePage> {
         if (!snapshot.hasData) {
           return LinearProgressIndicator();
         } else {
-
           List<WorkoutInfo> info = snapshot.data.docs
               .map((documentSnapshot) =>
               WorkoutInfo.fromMap(documentSnapshot.data()))
@@ -195,3 +231,74 @@ class _WorkoutInfoHomePageState extends State<WorkoutInfoHomePage> {
     );
   }
 }
+
+class DisplayCompletedLogs extends StatelessWidget {
+
+ final List<String> completedExercises = [];
+
+
+  Future<void> getCompletedExercises() async {
+
+    final QuerySnapshot result = await FirebaseFirestore.instance
+        .collection('test_user_info')
+        .doc('placeholder_uid')
+        .collection('completedLogs')
+        .get();
+
+    final List<DocumentSnapshot> customDoc = result.docs;
+
+    for (int i = 0; i < customDoc.length; i++) {
+      completedExercises.add(customDoc[i]["Name"]);
+    }
+
+    return completedExercises;
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Select a logged exercise"),
+        ),
+        body: FutureBuilder(
+        builder: (context, projectSnap) {
+      if (!projectSnap.hasData) {
+        return Container();
+      } else {
+        return Container(
+            child: ListView.builder(
+                itemBuilder: displayLogCards,
+                itemCount: completedExercises.length
+            )
+        );
+      }
+    },
+    future: getCompletedExercises(),
+    ),
+
+
+
+
+    );
+
+
+  }
+
+
+  Widget displayLogCards( BuildContext context, int index ){
+    return Card(
+      child: ListTile(
+          title: Text(completedExercises[index]),
+        onTap: ()
+          {
+            Scaffold.of(context).showSnackBar(
+                SnackBar(content: Text( "Selected exercise to be displayed: " +completedExercises[index])));
+          }
+      ),
+    );
+  }
+
+}
+
